fc315668b3e05c9de1d20ac297e4f21ab2f6a637
yaelito12 <yaelacostatorres@gmail.com>
2025-12-09 15:22:00 -0700

    Agregue estructura base de la clase Carta y enum TipoCarta

--- src/gflip7/comun/Carta.java
+++ src/gflip7/comun/Carta.java
@@ -1,13 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package gflip7.comun;
+package flip7.comun;
 
-/**
- *
- * @author Yaelll
- */
-public class Carta {
+import java.io.Serializable;
+
+public class Carta implements Serializable {
+    private static final long serialVersionUID = 1L;
     
+    public enum TipoCarta { 
+        NUMERO, 
+        MODIFICADOR, 
+        CONGELAR, 
+        VOLTEAR_TRES, 
+        SEGUNDA_OPORTUNIDAD 
+    }
+    
+    private TipoCarta tipo;
+    private int valor;
+    private boolean esX2;
+    
+    public Carta(TipoCarta tipo, int valor) {
+        this.tipo = tipo;
+        this.valor = valor;
+        this.esX2 = (tipo == TipoCarta.MODIFICADOR && valor == -1);
+    }
+    
+    public TipoCarta getTipo() { 
+        return tipo; 
+    }
+    
+    public int getValor() { 
+        return valor; 
+    }
+    
+    public boolean esX2() { 
+        return esX2; 
+    }
 }
