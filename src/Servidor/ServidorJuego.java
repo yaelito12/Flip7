652d9b2928e9c42aa826be6691e79a456003dc95
yaelito12 <yaelacostatorres@gmail.com>
2025-12-09 07:40:00 -0700

    inicializar servidor básico con socket en puerto 5555

--- src/Servidor/ServidorJuego.java
+++ src/Servidor/ServidorJuego.java
@@ -1,13 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Servidor;
+package flip7.servidor;
 
-/**
- *
- * @author Yaelll
- */
+import java.io.*;
+import java.net.*;
+import java.util.concurrent.*;
+
 public class ServidorJuego {
+    private static final int PUERTO = 5555;
+    private ServerSocket socketServidor;
+    private ExecutorService ejecutor = Executors.newCachedThreadPool();
+    private boolean ejecutando;
     
+    public void iniciar() {
+        try {
+            socketServidor = new ServerSocket(PUERTO);
+            ejecutando = true;
+            
+            System.out.println("========================================");
+            System.out.println("   SERVIDOR FLIP 7 - Puerto " + PUERTO);
+            System.out.println("========================================");
+            
+            while (ejecutando) {
+                try {
+                    Socket cliente = socketServidor.accept();
+                    System.out.println("+ Conexion: " + cliente.getInetAddress());
+                   
+                } catch (IOException e) {
+                    if (ejecutando) e.printStackTrace();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public static void main(String[] args) {
+        new ServidorJuego().iniciar();
+    }
 }
