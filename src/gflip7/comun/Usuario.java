5d55421cf96bc922ebd84bb5e137cc7099907990
yaelito12 <yaelacostatorres@gmail.com>
2025-12-09 18:17:14 -0700

    se agrega la estructura base de la clase Usuario con sus atributos principales y el constructor para el registro de nuevos usuarios

--- src/gflip7/comun/Usuario.java
+++ src/gflip7/comun/Usuario.java
@@ -1,13 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package gflip7.comun;
+package flip7.comun;
 
-/**
- *
- * @author Yaelll
- */
-public class Usuario {
+import java.io.Serializable;
+
+public class Usuario implements Serializable {
+    private static final long serialVersionUID = 1L;
     
+    private int id;
+    private String nombreUsuario;
+    private String contrasena;
+    private int partidasJugadas;
+    private int partidasGanadas;
+    private int puntajeTotal;
+    
+    public Usuario(String nombreUsuario, String contrasena) {
+        this.nombreUsuario = nombreUsuario;
+        this.contrasena = contrasena;
+        this.partidasJugadas = 0;
+        this.partidasGanadas = 0;
+        this.puntajeTotal = 0;
+    }
 }
