0d7c61aeb97b98d37872f6955ab8cdb0674ce27c
yaelito12 <yaelacostatorres@gmail.com>
2025-12-10 07:34:11 -0700

    estructura base de la clase SalaJuego con datos principales

--- src/gflip7/comun/SalaJuego.java
+++ src/gflip7/comun/SalaJuego.java
@@ -1,13 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package gflip7.comun;
+package flip7.comun;
 
-/**
- *
- * @author Yaelll
- */
-public class SalaJuego {
+import java.io.Serializable;
+import java.util.*;
+
+public class SalaJuego implements Serializable {
+    private static final long serialVersionUID = 1L;
     
+    private String idSala;
+    private String nombreSala;
+    private String nombreHost;
+    private int idHost;
+    private int maxJugadores;
+    private boolean juegoIniciado;
+    private long tiempoCreacion;
+    
+    public SalaJuego(String idSala, String nombreSala, String nombreHost, 
+                     int idHost, int maxJugadores) {
+        this.idSala = idSala;
+        this.nombreSala = nombreSala;
+        this.nombreHost = nombreHost;
+        this.idHost = idHost;
+        this.maxJugadores = maxJugadores;
+        this.tiempoCreacion = System.currentTimeMillis();
+    }
 }
