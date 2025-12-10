0ef91ec0965dec7147ed9709a6eb67bc17d1b705
yaelito12 <yaelacostatorres@gmail.com>
2025-12-09 20:59:33 -0700

    cree campos de jugador y estado en MensajeJuego

--- src/gflip7/comun/MensajeJuego.java
+++ src/gflip7/comun/MensajeJuego.java
@@ -11,57 +11,67 @@ public class MensajeJuego implements Serializable {
     public MensajeJuego(TipoMensaje tipo) {
         this.tipo = tipo;
     }
+
     public enum TipoMensaje { 
-    LOGIN, 
-    REGISTRO, 
-    LOGIN_EXITOSO, 
-    LOGIN_FALLIDO, 
-    REGISTRO_EXITOSO, 
-    REGISTRO_FALLIDO,
+        LOGIN, 
+        REGISTRO, 
+        LOGIN_EXITOSO, 
+        LOGIN_FALLIDO, 
+        REGISTRO_EXITOSO, 
+        REGISTRO_FALLIDO,
 
-    CONECTAR, 
-    DESCONECTAR, 
-    CONECTADO,
+        CONECTAR, 
+        DESCONECTAR, 
+        CONECTADO,
 
-    CREAR_SALA, 
-    UNIRSE_SALA, 
-    SALIR_SALA, 
-    OBTENER_SALAS, 
-    LISTA_SALAS, 
-    SALA_CREADA, 
-    SALA_UNIDA, 
-    SALA_ACTUALIZADA, 
-    SALA_SALIDA, 
-    ERROR_SALA,
+        CREAR_SALA, 
+        UNIRSE_SALA, 
+        SALIR_SALA, 
+        OBTENER_SALAS, 
+        LISTA_SALAS, 
+        SALA_CREADA, 
+        SALA_UNIDA, 
+        SALA_ACTUALIZADA, 
+        SALA_SALIDA, 
+        ERROR_SALA,
 
-    JUGADOR_UNIDO, 
-    JUGADOR_SALIO,
-    LISTO,
-    JUGADORES_LISTOS,
+        JUGADOR_UNIDO, 
+        JUGADOR_SALIO,
+        LISTO,
+        JUGADORES_LISTOS,
 
-    JUEGO_INICIA, 
-    RONDA_INICIA, 
-    TU_TURNO, 
-    FIN_RONDA, 
-    FIN_JUEGO,
+        JUEGO_INICIA, 
+        RONDA_INICIA, 
+        TU_TURNO, 
+        FIN_RONDA, 
+        FIN_JUEGO,
 
-    PEDIR, 
-    PLANTARSE, 
-    ASIGNAR_CARTA_ACCION,
+        PEDIR, 
+        PLANTARSE, 
+        ASIGNAR_CARTA_ACCION,
 
-    CARTA_REPARTIDA, 
-    JUGADOR_ELIMINADO, 
-    JUGADOR_PLANTADO, 
-    JUGADOR_CONGELADO, 
-    CARTA_ACCION_ROBADA, 
-    ELEGIR_OBJETIVO_ACCION,
+        CARTA_REPARTIDA, 
+        JUGADOR_ELIMINADO, 
+        JUGADOR_PLANTADO, 
+        JUGADOR_CONGELADO, 
+        CARTA_ACCION_ROBADA, 
+        ELEGIR_OBJETIVO_ACCION,
 
-    ESTADO_JUEGO, 
-    MENSAJE_CHAT, 
-    CHAT_DIFUSION,
-    ERROR,
+        ESTADO_JUEGO, 
+        MENSAJE_CHAT, 
+        CHAT_DIFUSION,
+        ERROR,
 
-    OBTENER_RANKINGS,
-    RESPUESTA_RANKINGS
-}
+        OBTENER_RANKINGS,
+        RESPUESTA_RANKINGS
+    }
+
+    private int idJugador;
+    private int idJugadorObjetivo;
+    private int numeroRonda;
+    private String nombreJugador;
+    private String mensaje;
+    private Carta carta;
+    private List<Jugador> jugadores;
+    private EstadoJuego estadoJuego;
 }
