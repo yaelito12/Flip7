66075928e610f80a6b54d0bc3fecafa355b8b1ce
yaelito12 <yaelacostatorres@gmail.com>
2025-12-09 17:28:00 -0700

    Cree la clase Mazo

---
+++ src/gflip7/comun/Mazo.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gflip7.comun;
+
+/**
+ *
+ * @author Yaelll
+ */
+public class Mazo {
+    
+}
