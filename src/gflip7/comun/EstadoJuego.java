3450ab808b2cb8db578cfdd2f60102d72f1505b7
yaelito12 <yaelacostatorres@gmail.com>
2025-12-11 18:08:03 -0700

    agregar método todosJugadoresEliminados() para validar fin de ronda/juego

--- src/gflip7/comun/EstadoJuego.java
+++ src/gflip7/comun/EstadoJuego.java
@@ -5,15 +5,15 @@ import java.util.*;
 
 public class EstadoJuego implements Serializable {
     private static final long serialVersionUID = 1L;
-    
-    public enum Fase { 
-        ESPERANDO_JUGADORES, 
-        REPARTIENDO, 
-        JUGANDO, 
-        FIN_RONDA, 
-        FIN_JUEGO 
+
+    public enum Fase {
+        ESPERANDO_JUGADORES,
+        REPARTIENDO,
+        JUGANDO,
+        FIN_RONDA,
+        FIN_JUEGO
     }
-    
+
     private List<Jugador> jugadores = new ArrayList<>();
     private int indiceJugadorActual;
     private int indiceRepartidor;
@@ -21,110 +21,124 @@ public class EstadoJuego implements Serializable {
     private int tamanoMazo;
     private int puntajeGanador = 200;
     private Fase fase = Fase.ESPERANDO_JUGADORES;
-   
-    public List<Jugador> getJugadores() { 
-        return jugadores; 
-    }
-    
-    public int getIndiceJugadorActual() { 
-        return indiceJugadorActual; 
-    }
-    
-    public void setIndiceJugadorActual(int i) { 
-        indiceJugadorActual = i; 
-    }
-    
-    public int getIndiceRepartidor() { 
-        return indiceRepartidor; 
-    }
-    
-    public void setIndiceRepartidor(int i) { 
-        indiceRepartidor = i; 
-    }
-    
-    public int getNumeroRonda() { 
-        return numeroRonda; 
-    }
-    
-    public void setNumeroRonda(int r) { 
-        numeroRonda = r; 
-    }
-    
-    public Fase getFase() { 
-        return fase; 
-    }
-    
-    public void setFase(Fase p) { 
-        fase = p; 
-    }
-    
-    public int getTamanoMazo() { 
-        return tamanoMazo; 
-    }
-    
-    public void setTamanoMazo(int s) { 
-        tamanoMazo = s; 
-    }
-    
-    public int getPuntajeGanador() { 
-        return puntajeGanador; 
+
+    public List<Jugador> getJugadores() {
+        return jugadores;
     }
 
-    public Jugador getJugadorActual() { 
+    public int getIndiceJugadorActual() {
+        return indiceJugadorActual;
+    }
+
+    public void setIndiceJugadorActual(int i) {
+        indiceJugadorActual = i;
+    }
+
+    public int getIndiceRepartidor() {
+        return indiceRepartidor;
+    }
+
+    public void setIndiceRepartidor(int i) {
+        indiceRepartidor = i;
+    }
+
+    public int getNumeroRonda() {
+        return numeroRonda;
+    }
+
+    public void setNumeroRonda(int r) {
+        numeroRonda = r;
+    }
+
+    public Fase getFase() {
+        return fase;
+    }
+
+    public void setFase(Fase p) {
+        fase = p;
+    }
+
+    public int getTamanoMazo() {
+        return tamanoMazo;
+    }
+
+    public void setTamanoMazo(int s) {
+        tamanoMazo = s;
+    }
+
+    public int getPuntajeGanador() {
+        return puntajeGanador;
+    }
+
+    public Jugador getJugadorActual() {
         if (indiceJugadorActual >= 0 && indiceJugadorActual < jugadores.size()) {
             return jugadores.get(indiceJugadorActual);
         }
-        return null; 
+        return null;
     }
-    
-    public Jugador getJugadorPorId(int id) { 
+
+    public Jugador getJugadorPorId(int id) {
         for (Jugador j : jugadores) {
             if (j.getId() == id) {
                 return j;
             }
         }
-        return null; 
+        return null;
     }
-    
-    public List<Jugador> getJugadoresActivos() { 
-        List<Jugador> activos = new ArrayList<>(); 
+
+    public List<Jugador> getJugadoresActivos() {
+        List<Jugador> activos = new ArrayList<>();
         for (Jugador j : jugadores) {
             if (j.estaActivo()) {
                 activos.add(j);
             }
         }
-        return activos; 
+        return activos;
     }
-    
-    public boolean esFinRonda() { 
+
+   
+    public boolean esFinRonda() {
         for (Jugador j : jugadores) {
             if (j.estaActivo()) {
                 return false;
             }
         }
-        return true; 
+        return true;
     }
-    
-    public boolean esFinJuego() { 
+
+    public boolean todosJugadoresEliminados() {
+        boolean hayJugadores = false;
+        for (Jugador j : jugadores) {
+            if (j.estaConectado()) {
+                hayJugadores = true;
+                if (!j.estaEliminado()) {
+                    return false;
+                }
+            }
+        }
+        return hayJugadores;
+    }
+
+    public boolean esFinJuego() {
         for (Jugador j : jugadores) {
             if (j.getPuntajeTotal() >= puntajeGanador) {
                 return true;
             }
         }
-        return false; 
+        return false;
     }
-    
-    public Jugador getGanador() { 
-        Jugador ganador = null; 
-        int maxPuntaje = -1; 
-        
+
+    public Jugador getGanador() {
+        Jugador ganador = null;
+        int maxPuntaje = -1;
+
         for (Jugador j : jugadores) {
-            if (j.getPuntajeTotal() > maxPuntaje) { 
-                maxPuntaje = j.getPuntajeTotal(); 
-                ganador = j; 
+            if (j.getPuntajeTotal() > maxPuntaje) {
+                maxPuntaje = j.getPuntajeTotal();
+                ganador = j;
             }
         }
-        
-        return ganador; 
+
+        return ganador;
     }
 }
