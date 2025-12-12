1408952b7728abfbd38ba54a0db8d0ab6612ab62
German Barron <germanbarrona12@gmail.com>
2025-12-12 02:11:53 -0700

    s

--- src/Servidor/ServidorJuego.java
+++ src/Servidor/ServidorJuego.java
@@ -461,17 +461,14 @@ class InstanciaSalaJuego implements LogicaJuego.EscuchaEventosJuego {
     jugadoresListos.clear();
     nombresJugadoresListos.clear();
     
-    // Reiniciar jugadores pero NO borrarlos
     for (Jugador j : logicaJuego.getEstadoJuego().getJugadores()) {
         j.reiniciarParaNuevoJuego();
     }
     
-    // NO eliminar la sala, solo marcarla como disponible
     servidor.alFinJuego(sala.getIdSala());
     
     System.out.println("[SALA] [" + sala.getIdSala() + "] lista para nuevo juego");
     
-    // Notificar a todos que la sala está lista para jugar de nuevo
     difundirActualizacionSala();
 }
     
